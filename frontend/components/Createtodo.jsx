58783bcf0149ac0c438f458f4537fa36fc4e5eea
Anandu797 <ananda.krishnanrj@gmail.com>
2026-05-14 15:19:39 +0530

    front end components to add task and render tasks created

--- frontend/components/Createtodo.jsx
+++ frontend/components/Createtodo.jsx
@@ -2,9 +2,22 @@ export function Createtodo() {
     return (
         <div>
 
-            <input type="text" placeholder="title">Title: </input>
-            <input type="text" placeholder="description">Description: </input>
-            <button>add task</button>
+            <input style={{
+                padding:10,
+                margin:10
+            }} type="text" placeholder="Title" />
+
+            <br></br>
+            <br></br>
+            <input type="text" style={{
+                padding:10,
+                margin:10
+            }} placeholder="Description" />
+            <br></br>
+            <br></br>
+            <button style={{
+                margin:10,padding:10,height:50
+            }}>add task</button>
 
 
         </div>
---
+++ frontend/components/Todos.jsx
@@ -0,0 +1,12 @@
+export function Todos(){
+    return (
+        <div style={{
+            margin:10,
+            padding:10
+        }}>
+            <h1>Go to gym</h1>
+            <h3>hit the gym today at 3 pm</h3>
+            <button>mark as done</button>
+        </div>
+    )
+}
